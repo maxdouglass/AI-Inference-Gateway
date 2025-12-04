67f462484494da8165997f1147164be04d342864
maxdouglass <77952746+maxdouglass@users.noreply.github.com>
2025-12-04 13:08:29 -0600

    Initial Setup

---
+++ src/main/java/com/max_douglass/inference_gateway/InferenceGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.max_douglass.inference_gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InferenceGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InferenceGatewayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/max_douglass/inference_gateway/InferenceGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.max_douglass.inference_gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InferenceGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
